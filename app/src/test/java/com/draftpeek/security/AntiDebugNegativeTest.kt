b084daf20751c79825742c176b61112866e24047
DraftPeek Dev <zengyangc@outlook.com>
2026-08-28 20:49:22 +0800

    fix(test): add missing assertNotNull import in AntiDebugNegativeTest

--- app/src/test/java/com/draftpeek/security/AntiDebugNegativeTest.kt
+++ app/src/test/java/com/draftpeek/security/AntiDebugNegativeTest.kt
@@ -7,6 +7,7 @@ import io.mockk.unmockkConstructor
 import java.io.File
 import org.junit.After
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
